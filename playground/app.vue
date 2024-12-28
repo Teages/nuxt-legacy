ae4a56985166b4e14e5b1b21e82e1e0d418e794a
Teages <Teages@teages.xyz>
2024-12-28 17:51:23 +0800

    chore: update playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,25 @@
-<script setup>
+<script setup lang="ts">
 </script>
 
 <template>
-  <div>
-    Nuxt module playground!
+  <div :class="$style.container">
+    <h1>@teages/nuxt-legacy</h1>
+    <DynamicImport />
+    <ObjectHasOwnPolyfill />
+
+    <div>
+      Try these browsers to visit the page:
+      <BrowserSuggestion />
+    </div>
+
+    <DecideIsLegacy />
   </div>
 </template>
+
+<style module>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+</style>
---
+++ playground/components/ObjectHasOwnPolyfill.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const target = { a: 1, b: 2, c: 3 }
+
+const { data, status } = useAsyncData(async () => {
+  return !!Object.hasOwn(target, 'a')
+}, { server: false })
+</script>
+
+<template>
+  <div>
+    If you see true below, it means Object.hasOwn works:
+    <div>
+      Does {{ JSON.stringify(target) }} have own property 'a'?
+      <span v-if="status === 'success'">{{ data }}</span>
+      <span v-else>Loading...</span>
+    </div>
+  </div>
+</template>

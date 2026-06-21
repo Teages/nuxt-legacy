49a3c68490d259720dd3ab2e620ba3af36428fa7
Teages <Teages@teages.xyz>
2026-06-21 23:24:38 +0800

    chore: add nuxt v5 playground

---
+++ playgrounds/v5/app/polyfills/1.abort-controller.ts
@@ -0,0 +1,67 @@
+/**
+ * IMPORTANT: Do not use it in production. This polyfill is for dev purposes only.
+ */
+
+setup(window)
+
+function setup(self: typeof window) {
+  const isPolyfillNeeded = !('AbortController' in self && typeof self.AbortController === 'function')
+  if (!isPolyfillNeeded) {
+    return
+  }
+
+  const SECRET = Symbol('abort signal secret')
+
+  class AbortSignal extends EventTarget {
+    _aborted = false
+    _onabort: ((this: AbortSignal, ev: Event) => any) | null = null
+
+    constructor(secret: unknown) {
+      if (secret !== SECRET) {
+        throw new TypeError('Illegal constructor')
+      }
+      super()
+    }
+
+    get aborted() {
+      return this._aborted
+    }
+
+    get onabort() {
+      return this._onabort
+    }
+
+    set onabort(handler) {
+      if (this._onabort) {
+        this.removeEventListener('abort', this._onabort)
+      }
+      this._onabort = handler
+      if (handler) {
+        this.addEventListener('abort', handler)
+      }
+    }
+  }
+
+  class AbortController {
+    private _signal: AbortSignal
+
+    constructor() {
+      this._signal = new AbortSignal(SECRET)
+    }
+
+    get signal() {
+      return this._signal
+    }
+
+    abort() {
+      if (this._signal.aborted) {
+        return
+      }
+      this._signal._aborted = true
+      const event = new Event('abort')
+      this._signal.dispatchEvent(event)
+    }
+  }
+
+  Object.assign(self, { AbortController, AbortSignal })
+}
